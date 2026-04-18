24378e1163e6471f5eaace85568970e519806977
Nash6643 <omarnash619@gmail.com>
2026-04-18 09:00:00 +0300

    feat(physics): introduce Axis-Aligned Bounding Box (AABB) primitive

---
+++ engine/physics/AABB.java
@@ -0,0 +1,37 @@
+package engine.physics;
+
+public class AABB {
+    private double x;
+    private double y;
+    private double width;
+    private double height;
+
+    public AABB(double x, double y, double width, double height) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+    }
+
+    public boolean intersects(AABB other) {
+        return (this.x < other.x + other.width &&
+                this.x + this.width > other.x &&
+                this.y < other.y + other.height &&
+                this.y + this.height > other.y);
+    }
+
+    public void setPosition(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void setSize(double width, double height) {
+        this.width = width;
+        this.height = height;
+    }
+
+    public double getX() { return x; }
+    public double getY() { return y; }
+    public double getWidth() { return width; }
+    public double getHeight() { return height; }
+}
